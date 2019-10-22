827b25e61e2bfb378d492d1e1be382206878214e
lylegold <godinglyle@gmail.com>
2019-10-21 23:53:46 -0500

    Added ex2 files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,24 @@
+var click = false;
 function show_board(){
     var board = document.getElementById("board").children;
     for (let x=0; x<board.length; x++){
         board[x].setAttribute("class", "square");
+
+        board[x].addEventListener("click",function(){
+            if (click == false){
+                board[x].setAttribute('class', 'square X');
+                board[x].innerHTML = 'X';
+                click=true;
+            }
+            else{
+                board[x].setAttribute('class', 'square O');
+                board[x].innerHTML = 'O';
+                click=false;
+            }
+
+        });
     }
-
 }
+
+
 window.onload = show_board;
-
-function gameplay(){
-    
-}
-
-
